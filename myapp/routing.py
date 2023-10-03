74f4bf1674f21684fa3f043279b20538afea5772
amadeus8701 <ehdud6495@gmail.com>
2023-10-04 03:36:42 +0900

    allowed host update

--- myapp/routing.py
+++
@@ -1,9 +0,0 @@
-from channels.routing import ProtocolTypeRouter, URLRouter
-from django.urls import path
-from . import consumers
-
-application = ProtocolTypeRouter({
-    "websocket": URLRouter([
-        path("ws://ec2-52-78-22-235.ap-northeast-2.compute.amazonaws.com:8080", consumers.MyConsumer.as_asgi()),
-    ]),
-})
