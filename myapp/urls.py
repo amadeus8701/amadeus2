75f3029829abe1f21220ce7874cae46240a90285
amadeus8701 <ehdud6495@gmail.com>
2023-10-04 03:43:21 +0900

    allowed host update

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,6 +1,7 @@
+# viewer/urls.py
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.display_file, name='display_file'),
+    path('', views.show_file, name='show_file'),
 ]
