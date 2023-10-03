ba79195c553d573d704ef49f9baacf1e62179cd4
amadeus8701 <ehdud6495@gmail.com>
2023-10-04 03:38:42 +0900

    allowed host update

--- myapp/views.py
+++ myapp/views.py
@@ -1,22 +1,7 @@
-import json
-import asyncio
+# viewer/views.py
 from django.shortcuts import render
-from django.http import HttpResponse
-from channels.generic.websocket import AsyncWebsocketConsumer
 
-def display_file(request):
+def show_file(request):
     with open('/home/ubuntu/srv/text_file.txt', 'r') as file:
-        content = file.read()
-    return render(request, 'myapp/display_file.html', {'content': content})
-
-class FileChangeConsumer(AsyncWebsocketConsumer):
-    async def connect(self):
-        await self.accept()
-
-    async def disconnect(self, close_code):
-        pass
-
-    async def send_file_change_notification(self, event):
-        await self.send(text_data=json.dumps({
-            'message': '파일이 변경되었습니다.'
-        }))
+        file_contents = file.read()
+    return render(request, 'myapp/display_file.html', {'file_contents': file_contents})
