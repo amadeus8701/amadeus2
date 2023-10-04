ff84cd53c93dc57f56347c4135f215c70597bdfe
amadeus8701 <ehdud6495@gmail.com>
2023-10-04 14:29:33 +0900

    allowed host update

--- myapp/views.py
+++ myapp/views.py
@@ -5,9 +5,11 @@ def display_text_file(request):
         content = file.read()
     return render(request, 'myapp/index.html', {'content': content})
 
-from django.http import JsonResponse
+from django.http import HttpResponse
 
 def poll_file_content(request):
     with open('/home/ubuntu/srv/text_file.txt', 'r') as file:
         content = file.read()
-    return JsonResponse({'content': content})
+
+    # 파일 내용을 그대로 반환합니다.
+    return HttpResponse(content, content_type='application/json')
