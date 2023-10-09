d625c7a94188f30aab87abfed445f029495f610b
amadeus8701 <ehdud6495@gmail.com>
2023-10-09 11:59:44 +0900

    allowed host update

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,8 +3,6 @@ from . import views
 from myapp.views import display_text_file
 
 urlpatterns = [
-    path('', display_text_file, name='display_text_file'),
-    path('', views.serve_image, name='serve_image'),
+    path('image/', views.serve_image, name='serve_image'),  # 이미지 표시 URL
+    path('text/', display_text_file, name='display_text_file'),  # 텍스트 파일 표시 URL
 ]
-
-
