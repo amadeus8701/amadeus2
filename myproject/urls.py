6c5b6d33a987ed75d59c72c573858fe7e3fdebd4
amadeus8701 <ehdud6495@gmail.com>
2023-10-09 12:48:25 +0900

    allowed host update

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from myapp.views import display_text_file, poll_file_content  # 'poll_file_content' 뷰 함수 추가
+from myapp.views import display_text_file, poll_file_content, serve_image
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('myapp.urls')),  # 'myapp.urls'를 include
-    path('poll_file/', poll_file_content, name='poll_file_content'),  # 'poll_file_content' 함수를 URL 패턴에 추가
+    path('', display_text_file, name='display_text_file'),
+    path('screenshot.jpg', serve_image, name='serve_image'),
+    path('poll_file/', poll_file_content, name='poll_file_content'),
 ]
