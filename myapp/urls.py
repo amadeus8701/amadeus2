290de3b4ce3e9c0d774e8f2ec408245b4508bf72
amadeus8701 <ehdud6495@gmail.com>
2023-10-09 11:30:23 +0900

    allowed host update

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,16 +1,10 @@
 from django.urls import path
+from . import views
 from myapp.views import display_text_file
 
 urlpatterns = [
     path('', display_text_file, name='display_text_file'),
-]
-
-
-
-
-from django.urls import path
-from . import views
-
-urlpatterns = [
     path('', views.serve_image, name='serve_image'),
 ]
+
+
