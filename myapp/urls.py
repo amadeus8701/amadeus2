387def18f15869971bc461a8cbcd6cf4ff31e845
amadeus8701 <ehdud6495@gmail.com>
2023-10-09 12:41:07 +0900

    allowed host update

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from myapp.views import serve_screenshot, display_text_file, poll_file_content
+from myapp.views import display_text_file, serve_image
 
 urlpatterns = [
-    path('screenshot.jpg', serve_screenshot, name='serve_screenshot'),
-    path('text_file', display_text_file, name='display_text_file'),
-    path('poll_text_file', poll_file_content, name='poll_file_content'),
+    path('', display_text_file, name='display_text_file'),
+    path('poll_file/', poll_file_content, name='poll_file_content'),
+    path('screenshot.jpg', serve_image, name='serve_image'),  # JPG 파일을 서빙하는 URL 패턴 추가
 ]
-
--- myapp/views.py
+++ myapp/views.py
@@ -1,33 +1,27 @@
-from django.http import HttpResponse
 from django.shortcuts import render
-import os
 
-def serve_screenshot(request):
-    # Linux PC에서의 JPG 파일 경로
-    screenshot_path = '/home/ubuntu/srv/screenshot.jpg'
-
-    # 파일이 있는지 확인합니다.
-    if os.path.exists(screenshot_path):
-        # JPG 파일을 열고 제공합니다.
-        with open(screenshot_path, 'rb') as file:
-            return FileResponse(file)
-    else:
-        # 파일이 존재하지 않는 경우 처리합니다.
-        return HttpResponse('파일을 찾을 수 없습니다', status=404)
-
-def display_text_file(request, file_path):
-    with open(file_path, 'r') as file:
+def display_text_file(request):
+    with open('/home/ubuntu/srv/text_file.txt', 'r') as file:
         content = file.read()
     return render(request, 'myapp/index.html', {'content': content})
 
+from django.http import HttpResponse
+
 def poll_file_content(request):
-    text_file_path = '/home/ubuntu/srv/text_file.txt'
-    with open(text_file_path, 'r') as file:
+    with open('/home/ubuntu/srv/text_file.txt', 'r') as file:
         content = file.read()
 
     # 파일 내용을 그대로 반환합니다.
     return HttpResponse(content, content_type='application/json')
 
 
+from django.http import HttpResponse
+import os
 
-    
+def serve_image(request):
+    image_path = '/home/ubuntu/srv/screenshot.jpg'  # JPG 파일의 경로
+    with open(image_path, 'rb') as image_file:
+        response = HttpResponse(image_file.read(), content_type='image/jpeg')
+    return response
+
+
