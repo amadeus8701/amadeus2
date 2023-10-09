946d823471cd3ac344cf2a4801ed5b97d94a5824
amadeus8701 <ehdud6495@gmail.com>
2023-10-09 13:09:28 +0900

    allowed host update

--- myapp/views.py
+++ myapp/views.py
@@ -19,23 +19,23 @@ def poll_file_content(request):
 
 from django.http import HttpResponse
 import os
-from datetime import datetime
+import random
 
 # ...
 
 def serve_image(request):
-    image_path = '/home/ubuntu/srv/screenshot.jpg'  # JPG 파일의 경로
+    image_path = '/home/ubuntu/srv/screenshot.jpg'
     with open(image_path, 'rb') as image_file:
         response = HttpResponse(image_file.read(), content_type='image/jpeg')
     
-    # 이미지 URL에 랜덤 파라미터를 추가하여 캐시 무효화를 수행합니다
-    response['Cache-Control'] = 'no-store, must-revalidate'
+    # 더 강력한 캐시 무효화 헤더를 설정합니다
+    response['Cache-Control'] = 'no-cache, no-store, must-revalidate'
     response['Pragma'] = 'no-cache'
     response['Expires'] = '0'
-    response['Cache-Control'] = 'public, max-age=0'
-    
-    # 이미지의 최종 수정 시간을 설정하여 브라우저가 변경을 감지하도록 합니다
-    image_modified_time = os.path.getmtime(image_path)
-    response['Last-Modified'] = datetime.utcfromtimestamp(image_modified_time).strftime('%a, %d %b %Y %H:%M:%S GMT')
+
+    # 랜덤 파라미터를 이미지 URL에 추가하여 캐시 무효화를 수행합니다
+    image_url = reverse('serve_image')
+    image_url += f'?{random.randint(0, 100000)}'  # 랜덤 파라미터를 추가합니다
+    response['Location'] = image_url
 
     return response
