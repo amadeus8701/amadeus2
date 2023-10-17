49bd3f3b8914671e88cf2968fc105597fc2afc2d
amadeus8701 <ehdud6495@gmail.com>
2023-10-17 09:38:13 +0900

    allowed host update

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,5 +4,5 @@ from myapp.views import display_text_file, serve_image
 urlpatterns = [
     path('', display_text_file, name='display_text_file'),
     path('poll_file/', poll_file_content, name='poll_file_content'),
-    path('screenshot.jpg', serve_image, name='serve_image'),  # JPG 파일을 서빙하는 URL 패턴 추가
+    path('screenshot.jpg', serve_image, name='serve_image'), 
 ]
